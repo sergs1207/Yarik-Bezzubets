bdf6f715f7fa5294b7bc1aa2df3fb4995d98918f
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-01-21 19:23:44 +0200

    update

---
+++ Lesson1/src/HairTrimmer.java
@@ -0,0 +1,22 @@
+
+public class HairTrimmer 
+
+{   
+	String brand; //назва виробника. Тип даних - текстовий рядок String.
+	String cuttingLengths;
+    int speeds;
+    int chargeTime;
+    boolean battery;
+    
+    void startTrimmer(){
+    	//ввімкнути тример
+}
+
+    void stopTrimmer(){
+    	///вимкнути тример
+}
+    
+    void openBatteryCase(){
+    	//вікрити кришку відсіку батереї
+}
+}
