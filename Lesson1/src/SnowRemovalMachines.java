57eff9589c5af9c60df611e4659ac3fcadf91951
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-01-21 18:57:21 +0200

    snow machines

---
+++ Lesson1/src/SnowRemovalMachines.java
@@ -0,0 +1,19 @@
+
+public class SnowRemovalMachines 
+
+{   
+	String brand; //назва виробника. Тип даних - текстовий рядок String.
+	String fuelType;
+    int speeds;
+    int weight;
+    boolean electricStart;  //електричний запуск, тип даних - булева змінна, приймаюча значення true чи false.
+    
+    void startCleaningSnow(){
+}
+
+    void stopCleaningSnow(){
+}
+    
+    void turnOnHeadlight(){
+}
+}
