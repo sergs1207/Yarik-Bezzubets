82bd597879f384d4d0c0e5b8cecb64ccbf8033dd
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-01-21 18:31:32 +0200

    wardrobe

---
+++ Lesson1/src/Wardrobe.java
@@ -0,0 +1,20 @@
+
+public class Wardrobe 
+
+{   
+	String brand; //назва виробника. Тип даних - текстовий рядок String.
+	String model; //назва моделі.
+	int height; //висота шафи, тип даних – цілі числа int.
+    int weight; //вага шафи, тип даних – цілі числа int.
+    boolean childrenDoorLock; //захиcт від вікриття дверей шафи дітьми, тип даних - булева змінна, приймаюча значення true чи false.
+    
+    void openDoor(){
+}
+    
+    void closeDoor(){
+    }
+    
+    void enableOpenDoorLight(){
+    	//ввімкнення підсвітки шафи, при відкритті дверей.
+    }
+}
