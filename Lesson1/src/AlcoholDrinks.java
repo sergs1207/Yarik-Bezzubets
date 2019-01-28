293541062bd7d1a22042c0c43550e0cf516182ac
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-01-28 10:08:37 +0200

    Atribute fix

--- Lesson1/src/AlcoholDrinks.java
+++ Lesson1/src/AlcoholDrinks.java
@@ -1,18 +1,18 @@
 package Lesson1;
 
 public class AlcoholDrinks {
-boolean isUsedInCocktail; // чи підходить алкогольний напій для поєднання із іншими напоями, тип даних - булева змінна, приймаюча значення true чи false.
-float bottleVolume; //об'єм бутилки.
-String alcoholCountry; //країна виробник.
-byte ABV; //вміст спирту в напої.
-String alcoholType; //тип спирту, з якого вироблено алкогольний напий, зерновий, виноградний і т.д. 
+	boolean isUsedInCocktail; // чи підходить алкогольний напій для поєднання із іншими напоями, тип даних - булева змінна, приймаюча значення true чи false.
+	float bottleVolume; //об'єм бутилки.
+	String alcoholCountry; //країна виробник.
+	byte ABV; //вміст спирту в напої.
+	String alcoholType; //тип спирту, з якого вироблено алкогольний напий, зерновий, виноградний і т.д. 
    
-void startShakeaCocktail() {
+	void startShakeaCocktail() {
     }
 
-void startDrink(){
+	void startDrink(){
     }
 
-void stopDrink(){
+	void stopDrink(){
     }
 }
