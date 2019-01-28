b75d105179dab55b5e5972cf80514173a8c3f9d2
ybezzubets <46883181+ybezzubets@users.noreply.github.com>
2019-01-28 10:10:25 +0200

    Fix atributes name

--- Lesson1/src/Airplane.java
+++ Lesson1/src/Airplane.java
@@ -1,14 +1,14 @@
 package Lesson1;
 
-public class Airplane {   
+public class Airplane {
+	boolean refuelingInTheAir;  //атрибут можливості заправки літака в повітрі, тип даних - булева змінна, приймаюча значення true чи false.
 	String airplanePurpose; //класифікація літаків за призначенням, військові, пасажирські, вантажні тощо. Тип даних - текстовий рядок String.
+	int airplineIssueDate; //дата випуску, тип даних – цілі числа int.
 	String airplaneFuelType; // тип пального.
 	int airplineNumberOfEngines; //кількість двигунів, тип даних – цілі числа int.
-    int airplineIssueDate; //дата випуску, тип даних – цілі числа int.
     int airplineWeight; //вага літака, тип даних – цілі числа int.
     int emergencyExit; //аварійні виходи.
     int passengerCompartment; // Кількість пасажирських місць.
-    boolean refuelingInTheAir;  //атрибут можливості заправки літака в повітрі, тип даних - булева змінна, приймаюча значення true чи false.
     
     void activateAutopilot(){
     }
